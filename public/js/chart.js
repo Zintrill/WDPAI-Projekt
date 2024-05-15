fa932ef23c6f63a372b88f61461bb38f8e6cd015
Zintrill <krolik2325@gmail.com>
2024-05-15 20:46:13 +0200

    Dashboard

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,70 +1,98 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Funkcja do generowania wykresu kołowego
-    function generatePieChart(chartId, chartData, chartName) {
-        const config = {
-            type: 'pie',
-            data: chartData,
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: { display: true, position: 'left', labels: { usePointStyle: true, padding: 20, color: 'white', font: { size: 20, weight: 'bold' } } },
-                    title: { display: true, text: chartName, position: 'top', font: { size: 20, weight: 'bold', color: 'white' } },
-                    datalabels: {
-                        formatter: (value, ctx) => {
-                            const sum = ctx.chart.data.datasets[0].data.reduce((acc, val) => acc + val, 0);
-                            const percentage = Math.round((value / sum) * 100);
-                            return `${percentage}% (${ctx.dataset.data[ctx.dataIndex]})`;
-                        },
-                        color: 'white',
-                        font: { weight: 'bold' },
-                        anchor: 'end',
-                        align: 'start',
-                        offset: -10
-                    }
+// Dane dla pierwszego wykresu donut
+
+Chart.defaults.color = '#FFF';
+
+const data1 = {
+    labels: ['Online', 'Offline', 'Waiting'],
+    datasets: [{
+        data: [200, 100, 150],
+        backgroundColor: [
+            '#7FC008', // Zielony
+            '#DB303F', // Czerwony
+            '#F68D2B' // Pomarańczowy
+        ],
+        borderWidth: 0, // Usunięcie białego obramowania
+        hoverOffset: 4
+    }]
+};
+
+// Dane dla drugiego wykresu donut
+const data2 = {
+    labels: ['Online', 'Offline', 'Waiting'],
+    datasets: [{
+        data: [80, 120, 90],
+        backgroundColor: [
+            '#7FC008', // Zielony
+            '#DB303F', // Czerwony
+            '#F68D2B' // Pomarańczowy
+        ],
+        borderWidth: 0, // Usunięcie białego obramowania
+        hoverOffset: 4
+    }]
+};
+
+// Opcje dla obu wykresów
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'bottom',
+            labels: {
+                font: {
+                    size: 15, // Zwiększenie czcionki legendy
                 },
-                layout: { padding: 0 },
-                hover: { mode: 'nearest', intersect: false },
-                tooltips: { enabled: false },
-                cutout: '40%'
+                usePointStyle: true, // Użycie kółek zamiast prostokątów w legendzie
+                padding: 20
+            }
+        },
+        title: {
+            display: true,
+            text: 'Switches',
+            font: {
+                size: 20,
+                weight: 'bold'
             },
-        };
-
-        const statusChart = new Chart(document.getElementById(chartId), config);
-
-        function addTotalText() {
-            const totalText = document.createElement('div');
-            const total = chartData.datasets[0].data.reduce((acc, val) => acc + val, 0);
-            totalText.innerHTML = `<div>Total</div><div>${total}</div>`;
-            totalText.classList.add('total-text');
-            document.getElementById(chartId).parentElement.appendChild(totalText);
+            padding: 20
         }
-
-        /*addTotalText();*/
     }
+};
 
-    // Dane dla pierwszego wykresu
-    const firstChartData = {
-        labels: ['Online', 'Offline', 'Waiting'],
-        datasets: [{
-            data: [80, 30, 20],
-            backgroundColor: ['#7FC008', '#DB303F', '#F68D2B'],
-            borderWidth: 0
-        }]
-    };
+// Opcje dla drugiego wykresu
+const options2 = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'bottom',
+            labels: {
+                font: {
+                    size: 15, // Zwiększenie czcionki legendy
+                },
+                usePointStyle: true, // Użycie kółek zamiast prostokątów w legendzie
+                padding: 20
+            }
+        },
+        title: {
+            display: true,
+            text: 'All',
+            font: {
+                size: 20,
+                weight: 'bold'
+            },
+            padding: 20
+        },
+    },
+};
 
-    // Generowanie pierwszego wykresu
-    generatePieChart('firstChart', firstChartData, 'Switches - Chart 1');
+// Utwórz pierwszy wykres donut
+const donutChart1 = new Chart(document.getElementById('donutChart1'), {
+    type: 'doughnut',
+    data: data1,
+    options: options
+});
 
-    // Dane dla drugiego wykresu
-    const secondChartData = {
-        labels: ['Online', 'Offline', 'Waiting'],
-        datasets: [{
-            data: [80, 50, 40],
-            backgroundColor: ['#7FC008', '#DB303F', '#F68D2B'],
-            borderWidth: 0
-        }]
-    };
-
-    // Generowanie drugiego wykresu
-    generatePieChart('secondChart', secondChartData, 'Switches - Chart 2');
+// Utwórz drugi wykres donut
+const donutChart2 = new Chart(document.getElementById('donutChart2'), {
+    type: 'doughnut',
+    data: data2,
+    options: options2
 });
